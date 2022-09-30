46a8c54be33d9282b0a8f76f91283cfb9631573b
haixiangyan <haixiang6123@gmail.com>
2022-09-30 15:58:55 +0800

    feat: 添加更多注释

--- load/index.js
+++ load/index.js
@@ -1,11 +1,13 @@
+// 一些正则表达式，可以不用管
 const ALL_SCRIPT_REGEX = /(<script[\s\S]*?>)[\s\S]*?<\/script>/gi;
 const SCRIPT_SRC_REGEX = /.*\ssrc=(['"])?([^>'"\s]+)/;
-const SCRIPT_ENTRY_REGEX = /.*\sentry\s*.*/;
 const LINK_TAG_REGEX = /<(link)\s+.*?>/isg;
 const STYLE_HREF_REGEX = /.*\shref=(['"])?([^>'"\s]+)/;
 
+// 判断是否为内联代码
 const isInlineCode = code => code.startsWith('<');
 
+// 获取内联代码的文本内容
 const getInlineCode = (match) => {
   const start = match.indexOf('>') + 1;
   const end = match.lastIndexOf('<');
@@ -15,8 +17,12 @@ const getInlineCode = (match) => {
 // 将 <link> 转为 <style>/* CSS 代码 */ xx </style>
 const genLinkReplaceSymbol = (linkHref, preloadOrPrefetch = false) => `<!-- ${preloadOrPrefetch ? 'prefetch/preload' : ''} link ${linkHref} replaced by import-html-entry -->`;
 
+// * 匹配 HTML 中的 <link> 以及 <script>
+// * 将 <link> 转换为注释 <!-- href ->
+// * 收集外部 <link> 的 href 地址
+// * 收集内联 <script> 代码以及外部 <script> 的 src 地址
 const processTpl = (html) => {
-  let styles = [], scripts = [], entry = '';
+  let styles = [], scripts = [];
 
   const template = html
     // 匹配 <link> 标签
@@ -34,16 +40,12 @@ const processTpl = (html) => {
     .replace(ALL_SCRIPT_REGEX, (match, scriptTag) => {
       // 如果是外部的 script，通过是否包含 src 来判断
       if (scriptTag.match(SCRIPT_SRC_REGEX)) {
-        // JS 入口 <script entry />
-        const matchedScriptEntry = scriptTag.match(SCRIPT_ENTRY_REGEX);
         // <script src = "xx" />
         const matchedScriptSrcMatch = scriptTag.match(SCRIPT_SRC_REGEX);
         // 脚本地址
         let matchedScriptSrc = matchedScriptSrcMatch && matchedScriptSrcMatch[2];
         // 记录 script 的 src 地址
         scripts.push(matchedScriptSrc);
-        // 记录入口（要么是匹配的入口，否则为最后一个 JS：当匹配到最后一个 JS 时，还没有 entry，那么最后一个 JS 视为入口）
-        entry = entry || matchedScriptEntry && matchedScriptSrc;
         return match;
       }
       // 此时为内联 script 的情况
@@ -53,9 +55,10 @@ const processTpl = (html) => {
       }
     })
 
-  return { template, scripts, styles, entry }
+  return { template, scripts, styles }
 }
 
+// 通过 fetch 发 HTTP 请求获取外部 CSS 代码
 const getExternalStyleSheets = (styles) => {
   return Promise.all(styles.map(styleLink => {
       return fetch(styleLink).then(response => response.text());
@@ -63,7 +66,9 @@ const getExternalStyleSheets = (styles) => {
   ));
 }
 
-function getExternalScripts(scripts) {
+// 通过 fetch 发 HTTP 请求获取外部 JS 代码
+// 如果是内联代码，那么直接返回
+const getExternalScripts = (scripts) => {
   // 遍历所有 script 的 src 地址
   return Promise.all(scripts.map(script => {
       // 字符串，要不是链接地址，要不是脚本内容（代码）
@@ -78,6 +83,7 @@ function getExternalScripts(scripts) {
   ));
 }
 
+// 将 <link> 的 CSS 代码解出来放到 HTML 中
 const getEmbedHtml = (template, styles) => {
   let embedHTML = template;
 
@@ -92,6 +98,7 @@ const getEmbedHtml = (template, styles) => {
     });
 }
 
+// 在沙箱中执行 JS 代码
 const execScripts = (scripts) => {
   // 激活沙箱
   window.sandbox.active();
@@ -113,6 +120,7 @@ const execScripts = (scripts) => {
 window.sandbox = new SnapshotSandbox();
 window.proxy = window.sandbox.proxy;
 
+// 加载微应用逻辑
 const loadMicroApp = async (containerSelector, name, url) => {
   // 获取 HTML
   const html = await (await fetch(url)).text();
@@ -135,6 +143,7 @@ const loadMicroApp = async (containerSelector, name, url) => {
   execScripts(scripts);
 }
 
+// 卸载微应用逻辑
 const unloadMicroApp = async (containerSelector) => {
   // 清空内容
   document.querySelector(containerSelector).innerHTML = '';
